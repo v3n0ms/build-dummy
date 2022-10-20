97932e56c5bd218b09c247fb984064edc9d88903
ramchennuru <ramprasad.chennuru@thoughtworks.com>
2022-10-20 09:22:30 +0530

    Ramprasad add files

---
+++ src/main/java/com/POSG/PosG/PosGApplication.java
@@ -0,0 +1,13 @@
+package com.POSG.PosG;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PosGApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PosGApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/POSG/PosG/PosGApplicationTests.java
@@ -0,0 +1,13 @@
+package com.POSG.PosG;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosGApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
